a7516284b98c02f83308658383cbe0a36ad60ea1
sokar <sokarus21@gmail.com>
2024-10-22 22:57:10 +0400

    Fixing migration

--- backend/migrations/000002_create_photosessions_table.up.sql
+++ backend/migrations/000002_create_photosessions_table.up.sql
@@ -8,5 +8,5 @@ create table
         type varchar(100) not null,
         created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
         updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        CONSTRAINT unique_folder_name UNIQUE (folder_name),
+        CONSTRAINT unique_folder_name UNIQUE (folder_name)
     );
